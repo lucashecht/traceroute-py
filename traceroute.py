d9de5935e0b963afa58fbd19a5c579a3bd161a47
Lucas Hecht <lucas.hecht@agdsn.de>
2020-11-24 22:14:42 +0100

    Show intermediate hops

--- traceroute.py
+++ traceroute.py
@@ -1,9 +1,11 @@
 from scapy.all import *
 
-def check_hops(dst):
+def check_hops(dst, verbose=False):
     p = IP(dst=dst, ttl=1)/ICMP()
     while(True):
-        answ = sr1(p)
+        answ = sr1(p, verbose=0)
+        if verbose:
+            print(f"{p.ttl}: {answ.src}")
         if answ.src == dst:
             return p.ttl
         else:
